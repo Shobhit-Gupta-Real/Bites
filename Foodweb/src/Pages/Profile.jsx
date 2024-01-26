a3b630640dafb31b84d4a83e34f9822b2bda3a01
shobhit <guptashobhit2024@gmail.com>
2024-01-27 00:54:11 +0530

    fourth

--- Foodweb/src/Pages/Profile.jsx
+++ Foodweb/src/Pages/Profile.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useUserContext } from '../Context/UserContext'
 import { Navigate } from 'react-router-dom'
 
 function Profile() {
-    const [dp, setDp] = useState()
+    const [dp, setDp] = useState({})
     const {user, setUserInfo} = useUserContext()
     const [nuser, setNuser] = useState(user.username)
     
-    useEffect(() => {
+    useCallback(() => {
       const fetchData = async () => {
         try {
           const response = await fetch('http://localhost:4000/profile', {
@@ -26,7 +26,17 @@ function Profile() {
       };
     
       fetchData();
-    }, []);
+    }, [setUserInfo]);
+
+    if(user.id){
+      fetch(`http://localhost:4000/${user.id}`,{
+      credentials: 'include'
+    }).then(response => {
+      response.json().then(postInfo =>{
+        setDp(postInfo)
+      })
+    })
+  }
 
       async function update(ev){
         const data = new FormData(); //is creating a new instance of the FormData object in JavaScript.
@@ -48,15 +58,18 @@ function Profile() {
     <div className='profile_page'>
         <h1>Account Settings</h1>
         <div className="profile_edit">
-        <svg xmlns="http://www.w3.org/2000/svg" id='profile_image' fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-         </svg>
+        {!dp.cover && (
+            <svg xmlns="http://www.w3.org/2000/svg" id='profile_image' fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+        )}
+        {dp.cover && <img src={`http://localhost:4000/${dp.cover}`} id='profile_dp' />}
          <label htmlFor="input_file">
          <svg xmlns="http://www.w3.org/2000/svg" id='edit_profile_image' style={{transform: "translateY(-50.5px)", translate: "-132px"}} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
 </svg></label>
         <input type="file" id='input_file' onChange={(e)=>setDp(e.target.files)}/>
-         <input type="text" value={nuser} onChange={(e) => setNuser(e.target.value)}/>
+         <input type="text" value={dp.username} onChange={(e) => setNuser(e.target.value)}/>
          <button className='funcbtn' onClick={update}>Update</button>
         </div>
     </div>
